297bdbbe76a85033247629546cdb79377c4eb7da
BennyAhlin <Benny.Ahlin@hotmail.com>
2024-01-16 15:18:06 +0100

    first commit

---
+++ Models.cs
@@ -0,0 +1,57 @@
+﻿namespace EntityFrameworkUppgift;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+public class BloggingContext : DbContext
+{
+    public DbSet<Blog> Blogs { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Post> Posts { get; set; }
+
+    public string DbPath { get; }
+
+    public BloggingContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = System.IO.Path.Join(path, "cs.forts-BennyAhlin.db");
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+}
+
+public class Blog
+{
+    public int BlogId { get; set; }
+    public string? Url { get; set; }
+    public string? Name { get; set; }
+
+    public List<Post> Posts { get; set; } = new List<Post>();
+
+}
+
+public class User
+{
+    public int UserId { get; set; }         
+    public string? Username { get; set; }
+    public string? Password { private get; set; }
+
+    public List<Post> Posts { get; set; } = new List<Post>();
+
+}
+
+public class Post
+{
+    public int PostId { get; set; }                 
+    public string? Title { get; set; }              
+    public string? Content { get; set; }            
+    public DateOnly PublishedOn { get; set; }       
+
+    public int BlogId { get; set; }
+    public Blog? Blog { get; set; }                 
+
+    public int UserId { get; set; }                    
+    public User? User { get; set; }  
+    
+}
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using EntityFrameworkUppgift;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.Design;
+
+
+string[] UserCSV = File.ReadAllLines("User.csv");
+string[] PostCSV = File.ReadAllLines("Post.csv");
+string[] BlogCSV = File.ReadAllLines("Blog.csv");
+
+using var db = new BloggingContext();
+db.Database.EnsureDeleted();
+db.SaveChanges();
+
+db.Database.EnsureCreated();
+db.SaveChanges();
+
+foreach (string line in UserCSV)
+{
+    string[] split = line.Split(",");
+    var alreadyExistCheck = db.Users.Find(int.Parse(split[0]));
+    if (alreadyExistCheck == null)
+    {
+        db.Add(new User { UserId = int.Parse(split[0]), Username = split[1], Password = split[2] });
+    }
+}
+db.SaveChanges();
+
+foreach (string line in BlogCSV)
+{
+    string[] split = line.Split(",");
+    var alreadyExistCheck = db.Blogs.Find(int.Parse(split[0]));
+    if (alreadyExistCheck == null)
+    {
+        db.Add(new Blog { BlogId = int.Parse(split[0]), Url = split[1], Name = split[2] });
+    }
+}
+db.SaveChanges();
+
+foreach (string line in PostCSV)
+{
+    string[] split = line.Split(",");
+
+    db.Add(new Post { Title = split[1], Content = split[2], PublishedOn = DateOnly.Parse(split[3]), BlogId = int.Parse(split[4]), UserId = int.Parse(split[5]) });
+}
+db.SaveChanges();
+
+foreach (User user in db.Users.OrderBy(u => u.Username))
+{
+
+    Console.WriteLine("-------------------------------");
+    Console.WriteLine();
+    Console.WriteLine("\t" + user.Username);
+    foreach (Post post in user.Posts.OrderBy(t => t.Title))
+    {
+        Console.WriteLine();
+        Console.WriteLine(post.Title + "\t " + post.Content + "\t " + post.PublishedOn + " \t");
+        Console.WriteLine(post.Blog?.Name);
+        Console.WriteLine(post.Blog?.Url);
+    }
+    Console.WriteLine();
+
+}
+db.SaveChanges();
+
+
+
+
+
